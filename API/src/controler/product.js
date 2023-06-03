116a8c5f3c5a49fb45c6c759eae0dd14a029d6ea
lyanhtuan2003~ <lyanhtuan21072003@gmail.com>
2023-06-03 15:33:16 +0700

    feat:NGLR-5-be-create-api-add-product  đã làm xong api add product  refs:NGLR-5

--- API/src/controler/product.js
+++ API/src/controler/product.js
@@ -1,1 +1,19 @@
-console.log("dfkad");
+import product from "../model/product";
+export const addProduct = async (req, res) => {
+    try {
+        const data = await product.create(req.body)
+        console.log(data)
+        if (!data) {
+            return res.status(400).json({
+                message: "không thêm được sản phẩm"
+            })
+        }
+        return res.status(200).json({
+            message: "thêm sản phẩm thành công", data
+        })
+    } catch (error) {
+        return res.status(400).json({
+            message: error
+        })
+    }
+}
--- API/src/router/product.js
+++ API/src/router/product.js
@@ -1,1 +1,8 @@
-console.log("dfkad");
+import express from "express"
+import { addProduct } from "../controler/product"
+
+const router = express.Router()
+
+router.post("/products", addProduct)
+
+export default router
