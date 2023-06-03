a1ae6b8d348c87dc6eb15427b63f0dd92af27fd5
lyanhtuan2003~ <lyanhtuan21072003@gmail.com>
2023-06-03 15:22:03 +0700

    feat:  NGLR-9-create-model-product + làm được model  product refs: NGLR-9

--- API/src/model/product.js
+++ API/src/model/product.js
@@ -1,1 +1,16 @@
-console.log("dfkad");
+import mongoose from "mongoose"
+const productSchame = mongoose.Schema({
+    name: String,
+    auther: String,
+    published: String,
+    description: String,
+    image: String,
+    price: Number,
+    quantity: Number,
+    date: String,
+    categoryId: {
+        type: mongoose.Types.ObjectId, ref: "category"
+    }
+
+}, { timestamps: true, versionKey: false })
+export default mongoose.model("product", productSchame)
