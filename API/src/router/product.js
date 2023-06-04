a3acdf2d5903f106a2dec5f5eafb9e95525fefa0
trungdvph19910 <trungdvph19910@fpt.edu.vn>
2023-06-04 15:56:35 +0700

    feat:   removeProduct- router-remove controller-remove refs:NGLR-3

--- API/src/router/product.js
+++ API/src/router/product.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addProduct, updateProduct } from "../controler/product";
+import { addProduct, removeProduct, updateProduct } from "../controler/product";
 
 const router = express.Router();
 
 router.post("/products", addProduct);
 router.put("/products/:id", updateProduct);
+router.delete("/products/:id", removeProduct)
 
 export default router;
