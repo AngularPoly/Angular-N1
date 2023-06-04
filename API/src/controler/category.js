5df26cfe9c8a652912ace209617c8dbc5c2d67a9
[Le Van Phuong] <[phuongpew@gmail.com]>
2023-06-04 12:17:00 +0700

    feat: Create API update Category refs: NGLR-11

--- API/src/controler/category.js
+++ API/src/controler/category.js
@@ -1,26 +1,48 @@
 import category from "../model/category";
 
 export const addCategory = async (req, res) => {
-    try {
-        const data = await category.create(req.body)
-        console.log(data)
-        if (!data) {
-            return res.status(400).json({ message: "không thêm được sản phẩm" })
-        }
-        return res.status(200).json({ message: "thêm sản phẩm thành công", data })
-    } catch (error) {
-        return res.status(400).json({ message: error })
+  try {
+    const data = await category.create(req.body);
+    console.log(data);
+    if (!data) {
+      return res.status(400).json({ message: "không thêm được danh mục" });
     }
-}
+    return res.status(200).json({ message: "thêm danh mục thành công", data });
+  } catch (error) {
+    return res.status(400).json({ message: error });
+  }
+};
 
 export const deleteCategory = async (req, res) => {
-    try {
-        const data = await category.findByIdAndDelete(req.params.id)
-        if (!data) {
-            return res.status(400).json({ message: "không xoá được sản phẩm" })
-        }
-        return res.status(200).json({ message: "xoá sản phẩm thành công", data })
-    } catch (error) {
-        return res.status(400).json({ message: error })
+  try {
+    const data = await category.findByIdAndDelete(req.params.id);
+    if (!data) {
+      return res.status(400).json({ message: "không xoá được danh mục" });
     }
-}
+    return res.status(200).json({ message: "xoá danh mục thành công", data });
+  } catch (error) {
+    return res.status(400).json({ message: error });
+  }
+};
+export const updateCategory = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const body = req.body;
+    const data = await category.findOneAndUpdate({ _id: id }, body, {
+      new: true,
+    });
+    if (!data) {
+      return res.status(400).json({
+        message: "Không cập nhập được danh mục",
+      });
+    }
+    return res.status(200).json({
+      message: "Cập nhập danh mục thành công",
+      data,
+    });
+  } catch (error) {
+    return res.json({
+      message: error.message,
+    });
+  }
+};
--- API/src/router/category.js
+++ API/src/router/category.js
@@ -1,6 +1,11 @@
 import express from "express";
-import { addCategory, deleteCategory } from "../controler/category";
+import {
+  addCategory,
+  deleteCategory,
+  updateCategory,
+} from "../controler/category";
 const router = express.Router();
 router.post("/categorys", addCategory);
 router.delete("/categorys/:id", deleteCategory);
+router.put("/categorys/:id", updateCategory);
 export default router;
