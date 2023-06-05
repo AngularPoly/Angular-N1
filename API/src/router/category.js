63889e26b3319e0dac7c50f2f30ea9d23d9c487a
[Le Van Phuong] <[phuongpew@gmail.com]>
2023-06-05 14:29:11 +0700

    feat: Create API CheckAuth refs: NGLR-19

--- API/src/router/category.js
+++ API/src/router/category.js
@@ -1,15 +1,16 @@
 import express from "express";
 import {
-    addCategory,
-    deleteCategory,
-    getAllCategory,
-    getOneCategory,
-    updateCategory,
+  addCategory,
+  deleteCategory,
+  getAllCategory,
+  getOneCategory,
+  updateCategory,
 } from "../controler/category";
+import { checkPermission } from "../middlewares/checkPermission";
 const router = express.Router();
 router.get("/categorys", getAllCategory);
 router.get("/categorys/:id", getOneCategory);
-router.post("/categorys", addCategory);
-router.delete("/categorys/:id", deleteCategory);
-router.put("/categorys/:id", updateCategory);
+router.post("/categorys", checkPermission, addCategory);
+router.delete("/categorys/:id", checkPermission, deleteCategory);
+router.put("/categorys/:id", checkPermission, updateCategory);
 export default router;
--- API/src/router/product.js
+++ API/src/router/product.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { addProduct, removeProduct, updateProduct,getAllProduct,getOneproduct } from "../controler/product";
-
+import {
+  addProduct,
+  removeProduct,
+  updateProduct,
+  getAllProduct,
+  getOneproduct,
+} from "../controler/product";
+import { checkPermission } from "../middlewares/checkPermission";
 const router = express.Router();
 
-router.post("/products", addProduct);
-router.put("/products/:id", updateProduct);
-router.delete("/products/:id", removeProduct);
+router.post("/products", checkPermission, addProduct);
+router.put("/products/:id", checkPermission, updateProduct);
+router.delete("/products/:id", checkPermission, removeProduct);
 router.get("/products", getAllProduct);
 router.get("/products/:id", getOneproduct);
 
