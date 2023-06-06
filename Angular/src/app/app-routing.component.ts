36332633c406c1209f6223563e52362a56667fde
lyanhtuan2003~ <lyanhtuan21072003@gmail.com>
2023-06-06 23:02:21 +0700

    style:ui-footer đã xây dựng xog ui footer refs:NGLR-26

--- Angular/src/app/app-routing.component.ts
+++ Angular/src/app/app-routing.component.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './layout/admin/admin.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { ClientComponent } from './layout/client/client.component';
 
 const routes: Routes = [
-  {path:"admin", component: AdminComponent, children:[
-    {path:"", redirectTo: "dashboard", pathMatch: "full"},
-    {path:"dashboard", component: DashboardComponent}
-  ]}
+  {
+    path: "", component: ClientComponent, children: [
+
+    ]
+  },
+  {
+    path: "admin", component: AdminComponent, children: [
+      { path: "", redirectTo: "dashboard", pathMatch: "full" },
+      { path: "dashboard", component: DashboardComponent }
+    ]
+  }
 ]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
