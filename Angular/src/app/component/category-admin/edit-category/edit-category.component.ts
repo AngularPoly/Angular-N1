8cdddd27db3bfeb9fa6d26f3714067439a88387d
trungdvph19910 <trungdvph19910@fpt.edu.vn>
2023-06-10 21:46:06 +0700

    feat:update-category chức năng sửa category refs:NGLR-43

--- Angular/src/app/component/category-admin/edit-category/edit-category.component.ts
+++ Angular/src/app/component/category-admin/edit-category/edit-category.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { ICategory } from 'src/app/interface/Category';
+import { ServiceService } from 'src/app/service/service.service';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-edit-category',
   templateUrl: './edit-category.component.html',
   styleUrls: ['./edit-category.component.scss']
 })
 export class EditCategoryComponent {
-
+  category: ICategory = {
+    name: "",
+  }
+  constructor(
+    private serviceService: ServiceService,
+    private route: ActivatedRoute) {
+    this.route.paramMap.subscribe(param => {
+      const id = param.get('id')
+      this.serviceService.getCategoryById(id).subscribe((product: any) => this.category = product.data)
+    })
+  }
+  onHandleSubmit() {
+    this.serviceService.updateCategory(this.category).subscribe(category => {
+      console.log(category);
+    })
+  }
 }
