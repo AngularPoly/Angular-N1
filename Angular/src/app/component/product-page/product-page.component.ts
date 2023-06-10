c31d68c28a2f2ac6d08ab26d8c569a5c7cfde576
lyanhtuan2003~ <lyanhtuan21072003@gmail.com>
2023-06-10 23:17:10 +0700

    feat:search-product làm xong chức năg tìm kiếm product refs:NGLR-48

--- Angular/src/app/component/product-page/product-page.component.ts
+++ Angular/src/app/component/product-page/product-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IProduct } from 'src/app/interface/Product';
+import { ServiceService } from 'src/app/service/service.service';
 
 @Component({
   selector: 'app-product-page',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-page.component.scss']
 })
 export class ProductPageComponent {
+  searchTerm: string = ''; // khai báo và khởi tạo biến searchTerm
+  searchResult: IProduct[] = [];
+
+  constructor(private productService: ServiceService) {
+    this.productService.getAllProduct().subscribe((product: any) => this.searchResult = product)
+  }
+
+  search() {
+    this.searchResult = this.searchResult.filter(product =>
+      product.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
 
 }
