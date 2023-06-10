045612a3f54e11ddba18c154928aaf4a01c69c23
lyanhtuan2003~ <lyanhtuan21072003@gmail.com>
2023-06-10 23:58:44 +0700

    feat:list-product-clien && search-product đã làm xong list product & sẻach còn 1 lỗi nhỏ nhưng cũng đã hoàn thành về phần chính refs:NGLR-48 && NGLR-46

--- Angular/src/app/component/product-page/product-page.component.ts
+++ Angular/src/app/component/product-page/product-page.component.ts
@@ -8,21 +8,23 @@ import { ICategory } from 'src/app/interface/Category';
   styleUrls: ['./product-page.component.scss']
 })
 export class ProductPageComponent {
-  searchTerm: string = ''; // khai báo và khởi tạo biến searchTerm
+  searchTerm = ''; // khai báo và khởi tạo biến searchTerm
   searchResult: IProduct[] = [];
-
+  categorys: ICategory[] = []
   constructor(private productService: ServiceService) {
-    this.productService.getAllProduct().subscribe((product: any) => this.searchResult = product)
+    this.productService.getAllProduct().subscribe((product: any) => this.searchResult = product.data)
+    this.productService.getAllCategory().subscribe((category: any) => this.categorys = category)
   }
 
   search() {
-    this.searchResult = this.searchResult.filter(product =>
-      product.name.toLowerCase().includes(this.searchTerm.toLowerCase())
-    );
+    if (this.searchTerm === "") {
+      window.location.reload() //chờ fix sau
+    }
+    else {
+      this.searchResult = this.searchResult.filter(product =>
+        product.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    }
   }
-  categorys: ICategory[] = []
-  constructor(private serviceService: ServiceService) {
-    this.serviceService.getAllCategory().subscribe((category: any) => this.categorys = category)
 
-  }
 }
