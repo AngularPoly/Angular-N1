2ca2e71913c6fcb1b57ea5c2b8db51ad38e48e13
trungdvph19910 <trungdvph19910@fpt.edu.vn>
2023-06-10 23:00:10 +0700

    feat: Ui list category refs:NGLR-47

--- Angular/src/app/component/product-page/product-page.component.ts
+++ Angular/src/app/component/product-page/product-page.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { ICategory } from 'src/app/interface/Category';
+import { ServiceService } from 'src/app/service/service.service';
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.scss']
 })
 export class ProductPageComponent {
+  categorys: ICategory[] = []
+  constructor(private serviceService: ServiceService) {
+    this.serviceService.getAllCategory().subscribe((category: any) => this.categorys = category)
 
+  }
 }
