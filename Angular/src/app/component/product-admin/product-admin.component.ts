d6cefd2241015eb9246a2cdffbbd07e9ffd8a2a1
lyanhtuan2003~ <lyanhtuan21072003@gmail.com>
2023-06-11 22:39:35 +0700

    feat:search-product-in-admin làm xong phần tím kiếm sản phẩm ở product refs:NGLR-50

--- Angular/src/app/component/product-admin/product-admin.component.ts
+++ Angular/src/app/component/product-admin/product-admin.component.ts
@@ -10,15 +10,27 @@ import { ServiceService } from '../../service/service.service';
 })
 export class ProductAdminComponent {
   products: IProduct[] = []
+  searchResult: IProduct[] = [];
+  searchTerm = ""
   constructor(private productService: ServiceService) {
-    this.productService.getAllProduct().subscribe((product: any) => this.products = product.data)
+    this.productService.getAllProduct().subscribe((product: any) => {
+      this.products = product.data
+      this.searchResult = this.products
+    })
   }
 
+  search() {
+    if (this.searchTerm === "") {
+      this.searchResult = this.products;
+    } else {
+      this.searchResult = this.products.filter(product =>
+        product.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    }
+  }
   onRemove(_id: any) {
     if (confirm("bạn có muốn xoá sản phẩm này không")) {
       this.productService.deleteProduct(_id).subscribe(() => this.products = this.products.filter((item) => item._id != _id))
-
     }
-
   }
 }
