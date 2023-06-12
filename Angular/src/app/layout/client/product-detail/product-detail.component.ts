78a1ae19a90ffa56bd6ea4f070629a37bb045044
lyanhtuan2003~ <lyanhtuan21072003@gmail.com>
2023-06-12 18:11:19 +0700

    feat: product-detail-client đã làm xong product-detail refs: NGLR-55

--- Angular/src/app/layout/client/product-detail/product-detail.component.ts
+++ Angular/src/app/layout/client/product-detail/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/interface/Product';
+import { ServiceService } from 'src/app/service/service.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent {
-
+  product: IProduct = {
+    name: "",
+    auther: "",
+    published: "",
+    description: "",
+    image: "",
+    price: 0,
+    quantity: 0,
+    date: "",
+  }
+  constructor(private productService: ServiceService, private route: ActivatedRoute) {
+    this.route.paramMap.subscribe(params => {
+      const id = params.get("id")
+      this.productService.getOneProduct(id).subscribe((data: any) => this.product = data.data)
+    })
+  }
 }
